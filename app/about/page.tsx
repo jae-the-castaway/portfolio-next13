bc0044b9f8f01485b9e8c66be2cdf83dd6084658
Jaeheon Kim <jaeheonkim95@gmail.com>
2023-03-14 20:25:27 -0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function About() {
+  return (
+    <main>
+        <h1>About Page</h1>
+    </main>
+  )
+}
