70e31ae17b6a8293956a3d4cece019cf7a4117ae
Jaeheon Kim <jaeheonkim95@gmail.com>
2023-03-27 09:17:24 -0500

    create on the side section

---
+++ app/about/on-the-side.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import LinkedinLogo from "../public/linkedin.svg";
+import GithubLogo from "../public/github.svg";
+import PaperplaneLogo from "../public/paper-plane-solid.svg";
+import Link from "next/link";
+import AngleRight from "../../public/angle-right-solid.svg";
+
+function OnTheSide() {
+  return <div></div>;
+}
+
+export default OnTheSide;
